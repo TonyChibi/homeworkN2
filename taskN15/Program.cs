e187f968ce4054c4f537d2d3747910bb365b123d
GlazHorsez <to5gogol@gmail.com>
2023-05-23 12:30:20 +0300

    added task n 15

---
+++ taskN15/Program.cs
@@ -0,0 +1,6 @@
+﻿//я не стал строку преобразовывать, пусть вводят что хотят
+Console.Clear();
+Console.Write("insert the numeral of a week day: ");
+string num=Console.ReadLine()!;
+if (num=="6"||num=="7") Console.WriteLine("yes");
+else Console.WriteLine("no");
