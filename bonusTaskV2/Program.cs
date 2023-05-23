8b0c801fb9dd43d04093420fe2d98d62ab536bf1
GlazHorsez <to5gogol@gmail.com>
2023-05-23 15:19:18 +0300

    added bonus task V2 with a array method

---
+++ bonusTaskV2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("insert set of integers divided by Space symbol: ");
+int[] numbers = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int max=0;
+int sec=0;
+for (int i=0; i<numbers.Length; i++){
+    if(numbers[i]>max){
+        sec=max;
+        max=numbers[i];
+    }
+    else if (numbers[i]>sec)sec=numbers[i];
+
+}
+Console.WriteLine($"the second most great integer is {sec}");
