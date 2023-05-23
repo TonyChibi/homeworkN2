e9af6295a5f1267756a54d9909d5859a91adf569
GlazHorsez <to5gogol@gmail.com>
2023-05-23 13:07:11 +0300

    added the bonus task, no array version

---
+++ bonusTask/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Clear();
+bool cond=true;
+int max=0;
+int second=0;
+do{
+    Console.Write("insert an integer or 'stop' to end the program:  ");
+    string insert = Console.ReadLine()!;
+    if (insert.ToLower()=="stop"){
+        cond=false;
+        Console.WriteLine($"the final second silver integer is {second}");
+    }
+    else{
+        int num= int.Parse(insert);
+        if (num>max){
+            second=max;
+            max=num;}
+        else if (num>second) second=num;
+
+        Console.WriteLine($"the current second greatest integer is {second}");
+    }
+}while(cond);
