f1eb7db35656ce5a8f683118bcc6bd095289818f
GlazHorsez <to5gogol@gmail.com>
2023-05-23 12:14:45 +0300

    add task N13

---
+++ taskN13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("insert some integer: ");
+int num=int.Parse(Console.ReadLine()!);
+int count=10;
+int mult=100;
+if (num/100==0){Console.WriteLine("Sorry, there is no third character");}
+else{
+    mult*=10;
+    while(num/mult!=0){
+        mult*=10;
+        count*=10;
+    };
+    Console.WriteLine((num%count)/(count/10));
+
+};
